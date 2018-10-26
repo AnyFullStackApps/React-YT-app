e494e2fcf5bed6c75021012dbcf25201e9082fd8
anygroup <anygroup@icloud.com>
2018-10-26 17:24:10 +0200

    onChange examples

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-class SearchBar extends React.Component{
+class SearchBar extends Component{
     render(){
-        return <input/>;
+        //return <input onChange={this.onInputChange}/>;
+        //OR short error functions
+        return <input onChange={event => console.log(event.target.value)}/>;
     }
+
+    // onInputChange(event){
+    //     console.log(event);
+    // }
 }
 
 export default SearchBar
