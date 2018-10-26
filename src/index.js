93c0679f90c3f276d68e922bfce6817a52fc5d2f
anygroup <anygroup@icloud.com>
2018-10-26 15:25:04 +0200

    new class component 'SearchBar'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
 
-const App = function(){
-  return <div>Hello</div>;
+const App = () => {
+  return <div>
+    <SearchBar/>
+  </div>;
 }
 
 ReactDOM.render(<App/>,document.querySelector('.container'));
